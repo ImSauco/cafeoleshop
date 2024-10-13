173ae780b7a17373d66b1fc94b75a328b7e6af84
Jaime <167428818+JaimeGomezBenito@users.noreply.github.com>
2024-10-13 19:34:16 +0200

    Init commit

---
+++ src/main/java/com/cafeoleclub/CafeoleclubApplication.java
@@ -0,0 +1,13 @@
+package com.cafeoleclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeoleclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeoleclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafeoleclub/CafeoleclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeoleclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeoleclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
